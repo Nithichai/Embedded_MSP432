f2185d15bc196aca6aed42db638265e70a56d78b
NopyJF <Nithichai@users.noreply.github.com>
2017-11-12 13:07:37 +0700

    Start by Nopy

---
+++ Objects/main.d
@@ -0,0 +1,12 @@
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432P4xx_DFP\3.2.1\Device\Include\msp.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432P4xx_DFP\3.2.1\Device\Include\msp432p401r.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432P4xx_DFP\3.2.1\Device\Include\msp_compatibility.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432P4xx_DFP\3.2.1\Device\Include\msp432p401r_classic.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.1.1\CMSIS\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.1.1\CMSIS\Include\cmsis_version.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.1.1\CMSIS\Include\cmsis_compiler.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.1.1\CMSIS\Include\cmsis_armcc.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.1.1\CMSIS\Include\mpu_armv7.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\TexasInstruments\MSP432P4xx_DFP\3.2.1\Device\Include\system_msp432p401r.h
---
+++ main.c
@@ -0,0 +1,65 @@
+#include "msp.h"
+#define SYSTICK_STCSR (*((volatile unsigned long *)0xE000E010))
+#define SYSTICK_STRVR (*((volatile unsigned long *)0xE000E014))
+#define SYSTICK_STCVR (*((volatile unsigned long *)0xE000E018))
+	
+void SysTick_Init(void);
+void SysTick_wait(uint32_t delay);
+void SysTick_wait1ms(uint32_t delay);
+void Port_Init(void);
+void Forward_Light(void);
+void Backward_Light(void);
+
+int main(void) {
+	SysTick_Init();
+	Port_Init();
+	while (1) {
+		if (P1OUT == 0x02) {
+			Forward_Light();
+		} else if (P1OUT == 0x10) {
+			Backward_Light();
+		}
+	}
+}
+
+void SysTick_Init(void) {
+	SYSTICK_STCSR = 0;
+	SYSTICK_STRVR = 0x00FFFFFF;
+	SYSTICK_STCVR = 0;
+	SYSTICK_STCSR = 0x00000005;
+}
+
+void SysTick_wait(uint32_t delay) {
+	SYSTICK_STRVR = delay - 1;
+	SYSTICK_STCVR = 0;
+	while ((SYSTICK_STCSR & 0x000100000) == 0) {
+	}
+}
+
+void Port_Init(void) {
+	P1SEL1 = 0x00;
+	P1SEL0 = 0x00;
+	P2SEL1 = 0x00;
+	P2SEL0 = 0x00;
+	P1DIR = 0x00;
+	P1REN = 0x12;
+	P1OUT = 0x12;
+	P2DIR = 0x07;
+	P2OUT = 0x00;
+}
+
+void Forward_Light(void) {
+	P2OUT = 0x00;
+	for (int i = 0; i < 3; i++) {
+		P2OUT = P2OUT << 1;
+		SysTick_wait(1000);
+	}
+}
+
+void Backward_Light(void) {
+	P2OUT = 0x04;
+	for (int i = 0; i < 3; i++) {
+		P2OUT = P2OUT >> 1;
+		SysTick_wait(1000);
+	}
+}
